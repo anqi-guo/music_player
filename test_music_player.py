19eb842cc6fcc53f0578e4637440e989409ad562
anqi-guo <guo.anqi@icloud.com>
2023-04-04 20:48:45 +0800

    added tests for display_music(), click_play(), and click_pause()

--- test_music_player.py
+++ test_music_player.py
@@ -1,40 +1,67 @@
-import time
-import tkinter as tk
-from music_player import MainApp
-import pygame
+import unittest
+from unittest.mock import patch
+from tkinter import Tk
+
+import pygame.mixer
+
+from music_player import MainApp, SONGS_DIR
 
 
-def test_main_app():
+class TestApp(unittest.TestCase):
 
-    # create main app
-    root = tk.Tk()
-    app = MainApp(root)
+    def setUp(self):
+        self.root = Tk()
+        self.app = MainApp(self.root)
 
-    # test GUI initialization
-    assert isinstance(app._song_list, tk.Listbox)
-    assert isinstance(app._buttons_frame, tk.Frame)
+    def tearDown(self):
+        self.root.destroy()
 
-    # test music functions
-    # play a song
-    app._click_play()
-    assert pygame.mixer.music.get_busy()
-    time.sleep(5)
+    def test_display_music(self):
+        # test if the song list is displayed correctly
+        expected_songs = ('song1.mp3', 'song2.mp3', 'song3.mp3')
+        self.app._song_list.delete(0, 'end')
+        for song in expected_songs:
+            self.app._song_list.insert('end', song)
+        displayed_songs = self.app._song_list.get(0, 'end')
+        self.assertTupleEqual(expected_songs, displayed_songs)
 
-    # pause the song
-    app._click_pause()
-    assert not pygame.mixer.music.get_busy()
+    @patch('tkinter.messagebox.showerror')
+    def test_play_music_with_no_song_selected(self, mock_messagebox):
+        # test if an error message is displayed when no song is selected to
+        # play
+        self.app._song_list.selection_clear(0, 'end')
+        self.app._click_play()
+        mock_messagebox.assert_called_once_with('show error',
+                                                'no song selected!')
 
-    # unpause the song
-    app._click_play()
-    assert pygame.mixer.music.get_busy()
-    time.sleep(5)
+    @patch('pygame.mixer.music.load')
+    @patch('pygame.mixer.music.play')
+    def test_play_music(self, mock_play, mock_load):
+        # test if the selected song is played
+        self.app._song_list.selection_clear(0, 'end')
+        self.app._song_list.insert('end', 'risk-136788.mp3')
+        self.app._song_list.selection_set('end')
+        self.app._click_play()
+        mock_load.assert_called_once_with(SONGS_DIR / 'risk-136788.mp3')
+        mock_play.assert_called_once_with()
 
-    # stop the song
-    app._click_pause()
-    assert not pygame.mixer.music.get_busy()
+    @patch('pygame.mixer.music.pause')
+    def test_pause_music(self, mock_pause):
+        # test if the playing music is paused
+        self.app._song_list.selection_clear(0, 'end')
+        self.app._song_list.insert('end', 'risk-136788.mp3')
+        self.app._song_list.selection_set('end')
+        self.app._click_play()
+        self.app._click_pause()
+        mock_pause.assert_called_once_with()
 
-    root.destroy()
+    @patch('tkinter.messagebox.showerror')
+    def test_pause_music_with_no_song_selected(self, mock_messagebox):
+        # test if an error message is displayed when no song is selected to
+        # pause
+        self.app._song_list.selection_clear(0, 'end')
+        self.app._click_pause()
+        mock_messagebox.assert_called_once_with('show error',
+                                                'no song selected!')
 
 
-if __name__ == "__main__":
-    test_main_app()

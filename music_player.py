f4d01c39a6d311a317cd54d99d0f8cd9fd96f68f
anqi-guo <guo.anqi@icloud.com>
2023-04-04 19:50:49 +0800

    moved load_image() function into MainApp class

--- music_player.py
+++ music_player.py
@@ -1,123 +1,127 @@
-from tkinter import *
-import pygame
-import os
+import tkinter as tk
 from tkinter import messagebox
+import pygame
+from pathlib import Path
+from typing import Dict
 
-SONGS_PATH = os.getcwd() + '/audio'
-IMAGE_PATH = os.getcwd() + '/logo'
-
-
-def get_images():
-    img_names = ['play', 'pause', 'next', 'prev', 'trash']
-    img_dict = {}
-    for name in img_names:
-        img_dict[name] = PhotoImage(file=f'{IMAGE_PATH}/{name}.png')
-    return img_dict
+SONGS_DIR = Path('audio')
+IMAGE_DIR = Path('logo')
 
 
 class MainApp:
     def __init__(self, parent):
-        self.parent = parent
-        self.song_list = Listbox(self.parent, bg='black', fg='white',
-                                 height=25,
-                                 width=55)
-        self.song_list.grid(row=0, column=0)
+        self._parent = parent
+        self._images = self._load_images()
+        self._current_song = None
+        self._paused = False
+        self._init_ui()
+        pygame.init()
 
-        self.buttons_frame = Frame(self.parent)
-        self.buttons_frame.grid(row=1, column=0)
+    def _init_ui(self):
+        self._song_list = tk.Listbox(self._parent,
+            bg='black', fg='white', height=25, width=55)
+        self._song_list.grid(row=0, column=0)
 
-        self.images = get_images()
-        self.current_song = None
-        self.paused = False
+        self._buttons_frame = tk.Frame(self._parent)
+        self._buttons_frame.grid(row=1, column=0)
 
-        # create buttons
         button_dict = {
-            'prev': [1, self.click_prev],
-            'play': [2, self.click_play],
-            'pause': [3, self.click_pause],
-            'next': [4, self.click_next],
-            'trash': [5, self.click_delete]
+            'prev': [1, self._click_prev],
+            'play': [2, self._click_play],
+            'pause': [3, self._click_pause],
+            'next': [4, self._click_next],
+            'trash': [5, self._click_delete]
         }
-        for k, v in button_dict.items():
-            Button(self.buttons_frame, image=self.images[k], command=v[1]) \
-                .grid(row=0, column=v[0], padx=10, pady=10)
 
-        # display the songs in the specified folder
-        self.display_music()
+        for name, (row, callback) in button_dict.items():
+            tk.Button(self._buttons_frame, image=self._images[name], command=callback) \
+                .grid(row=0, column=row, padx=10, pady=10)
 
-    def click_play(self):
-        """
-        check whether to play the music or not
-        :return: None
-        """
+        self._display_music()
+
+    def _load_images(self) -> Dict[str, tk.PhotoImage]:
+        img_names = ['play', 'pause', 'next', 'prev', 'trash']
+        img_dict = {}
+        for name in img_names:
+            img_dict[name] = tk.PhotoImage(file= IMAGE_DIR / f'{name}.png')
+        return img_dict
+
+    def _display_music(self):
+        # display songs to the song_list
+        for song in SONGS_DIR.glob('*.mp3'):
+            self._song_list.insert(tk.END, song.name)
+        # clear song box
+        self._song_list.selection_clear(0, tk.END)
+        # activate first song
+        self._song_list.activate(0)
+        # select first song
+        self._song_list.selection_set(0)
+
+    def _click_play(self):
         # check if a song is selected
-        if self.song_list.curselection() == ():
+        if self._song_list.curselection() == ():
             messagebox.showerror('show error', 'no song selected!')
         # if the song paused is same as the song selected
         # then unpause the song and update paused to False
-        elif self.current_song == self.song_list.get(ACTIVE) and self.paused:
-            self.unpause_music()
+        elif self._current_song == self._song_list.get(tk.ACTIVE) and self._paused:
+            self._unpause_music()
         # otherwise, just play the song selected
         # and update the current song to the song selected
         else:
-            self.play_music()
+            self._play_music()
 
-    def click_pause(self):
-        """
-        check whether to pause the music or not
-        :return: None
-        """
+    def _click_pause(self):
         # if no song is selected
-        if self.song_list.curselection() == ():
+        if self._song_list.curselection() == ():
             messagebox.showerror('show error', 'no song selected!')
         # if no song is playing
-        elif self.paused is True:
+        elif self._paused is True:
             messagebox.showerror('show error', 'no song is playing')
         # if a song is selected but the selected song is different from the
         # song playing
-        elif self.current_song != self.song_list.get(ACTIVE):
+        elif self._current_song != self._song_list.get(tk.ACTIVE):
             messagebox.showerror('show error',
                                  'the selected song is not playing!')
         # if a song is selected and the selected song is same as the
         # song playing then pause the music
         else:
-            self.pause_music()
+            self._pause_music()
 
-    def click_next(self):
+    def _click_next(self):
         """
         Play the song below the song selected
         :return: None
         """
         try:
             # get next song index
-            next_song_index = self.song_list.curselection()[0] + 1
+            next_song_index = self._song_list.curselection()[0] + 1
             # if current song is the last song
             # then the next song is the first song
-            if next_song_index == self.song_list.size():
+            if next_song_index == self._song_list.size():
                 next_song_index = 0
             # play next song
-            self.play_music_by_index(next_song_index)
+            self._play_music_by_index(next_song_index)
         except IndexError:
             messagebox.showerror('show error', 'No song selected!')
 
-    def click_prev(self):
+    def _click_prev(self):
         """
         Play the song above the song selected
         :return: None
         """
         try:
             # get prev song index
-            prev_song_index = self.song_list.curselection()[0] - 1
+            prev_song_index = self._song_list.curselection()[0] - 1
             # if current song is the first song
             # then previous song is the last song
             if prev_song_index == -1:
-                prev_song_index = self.song_list.size() - 1
+                prev_song_index = self._song_list.size() - 1
             # play previous song
-            self.play_music_by_index(prev_song_index)
+            self._play_music_by_index(prev_song_index)
         except IndexError:
             messagebox.showerror('show error', 'No song selected!')
 
-    def click_delete(self):
+    def _click_delete(self):
         """
         Delete the selected music from the Listbox
         :return: None
@@ -125,29 +129,28 @@ class MainApp:
         try:
             # if the song playing is same as the song to be deleted
             # then pause the song before deleting it and update paused to True
-            if self.current_song == self.song_list.get(ACTIVE) and \
-                    self.paused is False:
-                self.pause_music()
-            self.song_list.delete(self.song_list.curselection()[0])
+            if self._current_song == self._song_list.get(tk.ACTIVE) and \
+                    self._paused is False:
+                self._pause_music()
+            self._song_list.delete(self._song_list.curselection()[0])
         except IndexError:
             messagebox.showerror('show error', 'No song selected!')
 
-    def play_music(self):
+    def _play_music(self):
         """
         Play the current music
         :return: None
         """
         # get current song name
-        self.current_song = self.song_list.get(ACTIVE)
+        self._current_song = self._song_list.get(tk.ACTIVE)
         # load current song
-        pygame.mixer.music.load(os.path.join(
-            SONGS_PATH, self.current_song))
+        pygame.mixer.music.load(SONGS_DIR / self._current_song)
         # play current song
         pygame.mixer.music.play()
         # update paused status to False
-        self.paused = False
+        self._paused = False
 
-    def pause_music(self):
+    def _pause_music(self):
         """
         pause the music that was playing
         :return:
@@ -155,9 +158,9 @@ class MainApp:
         # pause the song
         pygame.mixer.music.pause()
         # update paused status to True
-        self.paused = True
+        self._paused = True
 
-    def unpause_music(self):
+    def _unpause_music(self):
         """
         unpause the song that was paused
         :return:
@@ -165,46 +168,28 @@ class MainApp:
         # unpause the song
         pygame.mixer.music.unpause()
         # update paused status to False
-        self.paused = False
+        self._paused = False
 
-    def play_music_by_index(self, song_index):
+    def _play_music_by_index(self, song_index):
         """
         activate the selected music
         :return:
         """
         # clear song_list
-        self.song_list.selection_clear(0, END)
+        self._song_list.selection_clear(0, tk.END)
         # activate next song
-        self.song_list.activate(song_index)
+        self._song_list.activate(song_index)
         # select next song
-        self.song_list.selection_set(song_index)
+        self._song_list.selection_set(song_index)
         # play the music
-        self.play_music()
-
-    def display_music(self):
-        """
-        Display songs in SONG_PATH onto song_list
-        :return: None
-        """
-        # display songs to the song_list
-        for song in os.listdir(SONGS_PATH):
-            if song.endswith(".mp3"):
-                self.song_list.insert(END, song)
-        # clear song box
-        self.song_list.selection_clear(0, END)
-        # activate first song
-        self.song_list.activate(0)
-        # select first song
-        self.song_list.selection_set(0)
+        self._play_music()
 
 
 def main():
     # create root window
-    root = Tk()
+    root = tk.Tk()
     root.title('Music Player')
     root.resizable(False, False)
-    # initialize mixer
-    pygame.mixer.init()
     # main app
     MainApp(root)
     # root mainloop

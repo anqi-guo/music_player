692a029abdd0bf3b49ae987df14c1598c9d37f83
anqi-guo <guo.anqi@icloud.com>
2023-04-07 22:43:18 +0800

    modified method names of MainApp class

--- music_player.py
+++ music_player.py
@@ -7,118 +7,121 @@ from typing import Dict
 SONGS_DIR = Path('audio')
 IMAGE_DIR = Path('logo')
 
+BUTTONS = {
+    'add': [1, 'click_add'],
+    'prev': [2, 'click_prev'],
+    'play': [3, 'click_play'],
+    'pause': [4, 'click_pause'],
+    'next': [5, 'click_next'],
+    'trash': [6, 'click_delete'],
+}
+
 
 class MainApp:
-    def __init__(self, parent):
-        self._parent = parent
-        self._images = self._load_images()
-        self._current_song = None
-        self._paused = False
+    def __init__(self, root):
+        self.root = root
+        self.image_dict = self.load_images()
+        self.current_song = None
+        self.paused = False
         self._init_ui()
-        pygame.init()
+        self._init_sound()
 
     def _init_ui(self):
-        self._song_list = tk.Listbox(self._parent,
-                                     bg='black', fg='white', height=25,
-                                     width=55)
-        self._song_list.grid(row=0, column=0)
+        self.song_list = tk.Listbox(self.root,
+                                    bg='black', fg='white', height=25,
+                                    width=55)
+        self.song_list.grid(row=0, column=0)
 
-        self._buttons_frame = tk.Frame(self._parent)
-        self._buttons_frame.grid(row=1, column=0)
+        self.buttons_frame = tk.Frame(self.root)
+        self.buttons_frame.grid(row=1, column=0)
 
-        button_dict = {
-            'add': [1, self._click_add],
-            'prev': [2, self._click_prev],
-            'play': [3, self._click_play],
-            'pause': [4, self._click_pause],
-            'next': [5, self._click_next],
-            'trash': [6, self._click_delete]
+        for name, (row, callback) in BUTTONS.items():
+            button = tk.Button(self.buttons_frame, image=self.image_dict[name],
+                               command=getattr(self, callback))
+            button.grid(row=0, column=row, padx=10, pady=10)
+
+    def _init_sound(self):
+        pygame.init()
+
+    def load_images(self) -> Dict[str, tk.PhotoImage]:
+        img_names = ['add', 'play', 'pause', 'next', 'prev', 'trash']
+        return {
+            name: tk.PhotoImage(file=IMAGE_DIR / f'{name}.png')
+            for name in img_names
         }
 
-        for name, (row, callback) in button_dict.items():
-            tk.Button(self._buttons_frame, image=self._images[name],
-                      command=callback) \
-                .grid(row=0, column=row, padx=10, pady=10)
-
-    def _load_images(self) -> Dict[str, tk.PhotoImage]:
-        img_names = ['add', 'play', 'pause', 'next', 'prev', 'trash']
-        img_dict = {}
-        for name in img_names:
-            img_dict[name] = tk.PhotoImage(file=IMAGE_DIR / f'{name}.png')
-        return img_dict
-
-    def _click_add(self):
+    def click_add(self):
         # add songs to the song_list
         song = filedialog.askopenfilename(initialdir=SONGS_DIR,
                                           title='Choose a song')
-        self._song_list.insert(tk.END, song[song.rfind('/') + 1:])
+        self.song_list.insert(tk.END, song[song.rfind('/') + 1:])
 
-    def _click_play(self):
+    def click_play(self):
         # check if a song is selected
-        if self._song_list.curselection() == ():
+        if self.song_list.curselection() == ():
             messagebox.showerror('show error', 'no song selected!')
         # if the song paused is same as the song selected
         # then unpause the song and update paused to False
-        elif self._current_song == self._song_list.get(
-                tk.ACTIVE) and self._paused:
-            self._unpause_music()
+        elif self.current_song == self.song_list.get(
+                tk.ACTIVE) and self.paused:
+            self.unpause_music()
         # otherwise, just play the song selected
         # and update the current song to the song selected
         else:
-            self._play_music()
+            self.play_music()
 
-    def _click_pause(self):
+    def click_pause(self):
         # if no song is selected
-        if self._song_list.curselection() == ():
+        if self.song_list.curselection() == ():
             messagebox.showerror('show error', 'no song selected!')
         # if no song is playing
-        elif self._paused is True:
+        elif self.paused is True:
             messagebox.showerror('show error', 'no song is playing!')
         # if a song is selected but the selected song is different from the
         # song playing
-        elif self._current_song != self._song_list.get(tk.ACTIVE):
+        elif self.current_song != self.song_list.get(tk.ACTIVE):
             messagebox.showerror('show error',
                                  'the selected song is not playing!')
         # if a song is selected and the selected song is same as the
         # song playing then pause the music
         else:
-            self._pause_music()
+            self.pause_music()
 
-    def _click_next(self):
+    def click_next(self):
         """
         Play the song below the song selected
         :return: None
         """
         try:
             # get next song index
-            next_song_index = self._song_list.curselection()[0] + 1
+            next_song_index = self.song_list.curselection()[0] + 1
             # if current song is the last song
             # then the next song is the first song
-            if next_song_index == self._song_list.size():
+            if next_song_index == self.song_list.size():
                 next_song_index = 0
             # play next song
-            self._play_music_by_index(next_song_index)
+            self.play_music_by_index(next_song_index)
         except IndexError:
             messagebox.showerror('show error', 'no song selected!')
 
-    def _click_prev(self):
+    def click_prev(self):
         """
         Play the song above the song selected
         :return: None
         """
         try:
             # get prev song index
-            prev_song_index = self._song_list.curselection()[0] - 1
+            prev_song_index = self.song_list.curselection()[0] - 1
             # if current song is the first song
             # then previous song is the last song
             if prev_song_index == -1:
-                prev_song_index = self._song_list.size() - 1
+                prev_song_index = self.song_list.size() - 1
             # play previous song
-            self._play_music_by_index(prev_song_index)
+            self.play_music_by_index(prev_song_index)
         except IndexError:
             messagebox.showerror('show error', 'no song selected!')
 
-    def _click_delete(self):
+    def click_delete(self):
         """
         Delete the selected music from the Listbox
         :return: None
@@ -126,28 +129,28 @@ class MainApp:
         try:
             # if the song playing is same as the song to be deleted
             # then pause the song before deleting it and update paused to True
-            if self._current_song == self._song_list.get(tk.ACTIVE) and \
-                    self._paused is False:
-                self._pause_music()
-            self._song_list.delete(self._song_list.curselection()[0])
+            if self.current_song == self.song_list.get(tk.ACTIVE) and \
+                    self.paused is False:
+                self.pause_music()
+            self.song_list.delete(self.song_list.curselection()[0])
         except IndexError:
             messagebox.showerror('show error', 'no song selected!')
 
-    def _play_music(self):
+    def play_music(self):
         """
         Play the current music
         :return: None
         """
         # get current song name
-        self._current_song = self._song_list.get(tk.ACTIVE)
+        self.current_song = self.song_list.get(tk.ACTIVE)
         # load current song
-        pygame.mixer.music.load(SONGS_DIR / self._current_song)
+        pygame.mixer.music.load(SONGS_DIR / self.current_song)
         # play current song
         pygame.mixer.music.play()
         # update paused status to False
-        self._paused = False
+        self.paused = False
 
-    def _pause_music(self):
+    def pause_music(self):
         """
         pause the music that was playing
         :return:
@@ -155,9 +158,9 @@ class MainApp:
         # pause the song
         pygame.mixer.music.pause()
         # update paused status to True
-        self._paused = True
+        self.paused = True
 
-    def _unpause_music(self):
+    def unpause_music(self):
         """
         unpause the song that was paused
         :return:
@@ -165,21 +168,21 @@ class MainApp:
         # unpause the song
         pygame.mixer.music.unpause()
         # update paused status to False
-        self._paused = False
+        self.paused = False
 
-    def _play_music_by_index(self, song_index):
+    def play_music_by_index(self, song_index):
         """
         activate the selected music
         :return:
         """
         # clear song_list
-        self._song_list.selection_clear(0, tk.END)
+        self.song_list.selection_clear(0, tk.END)
         # activate next song
-        self._song_list.activate(song_index)
+        self.song_list.activate(song_index)
         # select next song
-        self._song_list.selection_set(song_index)
+        self.song_list.selection_set(song_index)
         # play the music
-        self._play_music()
+        self.play_music()
 
 
 def main():
--- test_music_player.py
+++ test_music_player.py
@@ -1,6 +1,5 @@
 import unittest
 from unittest.mock import patch
-import pygame.mixer
 import tkinter as tk
 
 from music_player import MainApp, SONGS_DIR
@@ -16,27 +15,27 @@ class TestApp(unittest.TestCase):
         self.root.destroy()
 
     def test_load_images(self) -> None:
-        img_dict = self.app._load_images()
+        img_dict = self.app.load_images()
         self.assertIsInstance(img_dict, dict)
-        self.assertEqual(len(img_dict), 5)
+        self.assertEqual(len(img_dict), 6)
         for img in img_dict.values():
             self.assertIsInstance(img, tk.PhotoImage)
 
     def test_add_music(self):
         # test if music can be added to the listbox
         # check if listbox is empty
-        assert self.app._song_list.size() == 0
+        assert self.app.song_list.size() == 0
         # add a song to the listbox
-        self.app._click_add()
+        self.app.click_add()
         # check if song is added to the listbox
-        assert self.app._song_list.size() == 1
+        assert self.app.song_list.size() == 1
 
     @patch('tkinter.messagebox.showerror')
     def test_play_music_with_no_song_selected(self, mock_messagebox):
         # test if an error message is displayed when no song is selected to
         # play
-        self.app._song_list.selection_clear(0, 'end')
-        self.app._click_play()
+        self.app.song_list.selection_clear(0, 'end')
+        self.app.click_play()
         mock_messagebox.assert_called_once_with('show error',
                                                 'no song selected!')
 
@@ -44,38 +43,38 @@ class TestApp(unittest.TestCase):
     @patch('pygame.mixer.music.play')
     def test_play_music(self, mock_play, mock_load):
         # test if the selected song is played
-        self.app._song_list.insert('end', 'risk.mp3')
-        self.app._song_list.selection_set('end')
-        self.app._click_play()
+        self.app.song_list.insert('end', 'risk.mp3')
+        self.app.song_list.selection_set('end')
+        self.app.click_play()
         mock_load.assert_called_once_with(SONGS_DIR / 'risk.mp3')
         mock_play.assert_called_once_with()
 
     @patch('pygame.mixer.music.pause')
     def test_pause_music(self, mock_pause):
         # test if the playing music is paused
-        self.app._song_list.insert('end', 'risk.mp3')
-        self.app._song_list.selection_set('end')
-        self.app._click_play()
-        self.app._click_pause()
+        self.app.song_list.insert('end', 'risk.mp3')
+        self.app.song_list.selection_set('end')
+        self.app.click_play()
+        self.app.click_pause()
         mock_pause.assert_called_once_with()
 
     @patch('tkinter.messagebox.showerror')
     def test_pause_music_with_no_song_selected(self, mock_messagebox):
         # test if an error message is displayed when no song is selected to
         # pause
-        self.app._song_list.selection_clear(0, 'end')
-        self.app._click_pause()
+        self.app.song_list.selection_clear(0, 'end')
+        self.app.click_pause()
         mock_messagebox.assert_called_once_with('show error',
                                                 'no song selected!')
 
     @patch('tkinter.messagebox.showerror')
     def test_pause_music_with_no_song_playing(self, mock_messagebox):
         # Ensure that an error message is displayed when no song is playing
-        self.app._song_list.insert('end', 'risk.mp3')
-        self.app._song_list.selection_set('end')
-        self.app._click_play()
-        self.app._click_pause()
-        self.app._click_pause()
+        self.app.song_list.insert('end', 'risk.mp3')
+        self.app.song_list.selection_set('end')
+        self.app.click_play()
+        self.app.click_pause()
+        self.app.click_pause()
         mock_messagebox.assert_called_once_with('show error',
                                                 'no song is playing!')
 
@@ -83,13 +82,13 @@ class TestApp(unittest.TestCase):
     def test_pause_music_with_selected_song_not_playing(self, mock_messagebox):
         # Ensure that an error message is displayed when selected song is not
         # playing
-        self.app._song_list.insert('end', 'risk.mp3')
-        self.app._song_list.selection_set('end')
-        self.app._click_play()
-        self.app._song_list.insert('end', 'drop-it.mp3')
-        self.app._song_list.selection_set('end')
-        self.app._song_list.activate('end')
-        self.app._click_pause()
+        self.app.song_list.insert('end', 'risk.mp3')
+        self.app.song_list.selection_set('end')
+        self.app.click_play()
+        self.app.song_list.insert('end', 'drop-it.mp3')
+        self.app.song_list.selection_set('end')
+        self.app.song_list.activate('end')
+        self.app.click_pause()
         mock_messagebox.assert_called_once_with(
             'show error',
             'the selected song is not playing!')
@@ -98,11 +97,11 @@ class TestApp(unittest.TestCase):
     @patch('pygame.mixer.music.play')
     def test_click_next(self, mock_play, mock_load):
         # test if next song is played
-        self.app._song_list.insert('end', 'risk.mp3')
-        self.app._song_list.insert('end', 'drop-it.mp3')
-        self.app._song_list.selection_set(0)
-        self.app._song_list.activate(0)
-        self.app._click_next()
+        self.app.song_list.insert('end', 'risk.mp3')
+        self.app.song_list.insert('end', 'drop-it.mp3')
+        self.app.song_list.selection_set(0)
+        self.app.song_list.activate(0)
+        self.app.click_next()
 
         mock_load.assert_called_once_with(SONGS_DIR / 'drop-it.mp3')
         mock_play.assert_called_once_with()
@@ -111,11 +110,11 @@ class TestApp(unittest.TestCase):
     @patch('pygame.mixer.music.play')
     def test_click_prev(self, mock_play, mock_load):
         # test if previous song is played
-        self.app._song_list.insert('end', 'risk.mp3')
-        self.app._song_list.insert('end', 'drop-it.mp3')
-        self.app._song_list.selection_set(0)
-        self.app._song_list.activate(0)
-        self.app._click_prev()
+        self.app.song_list.insert('end', 'risk.mp3')
+        self.app.song_list.insert('end', 'drop-it.mp3')
+        self.app.song_list.selection_set(0)
+        self.app.song_list.activate(0)
+        self.app.click_prev()
 
         mock_load.assert_called_once_with(SONGS_DIR / 'drop-it.mp3')
         mock_play.assert_called_once_with()
@@ -123,19 +122,21 @@ class TestApp(unittest.TestCase):
     def test_click_delete(self):
         # test if the song is deleted
         # add a song
-        self.app._song_list.insert('end', 'risk.mp3')
-        self.app._song_list.selection_set('end')
-        self.app._song_list.activate('end')
+        self.app.song_list.insert('end', 'risk.mp3')
+        self.app.song_list.selection_set('end')
+        self.app.song_list.activate('end')
         # check if song is added to the listbox
-        assert self.app._song_list.size() == 1
+        assert self.app.song_list.size() == 1
         # delete the song
-        self.app._click_delete()
+        self.app.click_delete()
         # check if listbox is empty
-        assert self.app._song_list.size() == 0
+        assert self.app.song_list.size() == 0
 
     @patch('tkinter.messagebox.showerror')
     def test_click_delete_with_no_song_selected(self, mock_messagebox):
         # Ensure that an error message is displayed when no song is
-        self.app._click_delete()
+        self.app.click_delete()
         mock_messagebox.assert_called_once_with('show error',
                                                 'no song selected!')
+
+

8d5b0dd7abf817c5f6f254db308744c9fe273a60
anqi-guo <guo.anqi@icloud.com>
2023-04-15 16:22:37 +0800

    added comments

--- test_music_player.py
+++ test_music_player.py
@@ -18,6 +18,7 @@ class TestApp(unittest.TestCase):
         self.root.destroy()
 
     def test_load_images(self) -> None:
+        """test if images' format are changed to tk.PhotoImage"""
         img_dict = self.app.load_images()
         self.assertIsInstance(img_dict, dict)
         self.assertEqual(len(img_dict), 6)
@@ -25,6 +26,7 @@ class TestApp(unittest.TestCase):
             self.assertIsInstance(img, tk.PhotoImage)
 
     def test_add_music(self):
+        """test if music can be added"""
         # create a mock file dialog
         mock_file_dialog = Mock()
         mock_file_dialog.return_value = 'lifelike.mp3'
@@ -39,6 +41,7 @@ class TestApp(unittest.TestCase):
 
     @patch('tkinter.messagebox.showerror')
     def test_click_add_duplicate_song(self, mock_messagebox):
+        """test if error message will pop up after adding duplicated song"""
         # set up the test by adding a song to the song_list
         self.app.playlist.insert(tk.END, 'drop-it.mp3')
         # create a mock file dialog
@@ -56,8 +59,8 @@ class TestApp(unittest.TestCase):
 
     @patch('tkinter.messagebox.showerror')
     def test_play_music_with_no_song_selected(self, mock_messagebox):
-        # test if an error message is displayed when no song is selected to
-        # play
+        """"test if an error message is displayed when no song is selected to
+        play"""
         self.app.playlist.selection_clear(0, 'end')
         self.app.click_play()
         mock_messagebox.assert_called_once_with('show error',
@@ -66,7 +69,7 @@ class TestApp(unittest.TestCase):
     @patch('pygame.mixer.music.load')
     @patch('pygame.mixer.music.play')
     def test_play_music(self, mock_play, mock_load):
-        # test if the selected song is played
+        """test if the selected song is played"""
         self.app.playlist.insert('end', 'risk.mp3')
         self.app.playlist.selection_set('end')
         self.app.click_play()
@@ -75,7 +78,7 @@ class TestApp(unittest.TestCase):
 
     @patch('pygame.mixer.music.pause')
     def test_pause_music(self, mock_pause):
-        # test if the playing music is paused
+        """test if the playing music is paused"""
         self.app.playlist.insert('end', 'risk.mp3')
         self.app.playlist.selection_set('end')
         self.app.click_play()
@@ -84,8 +87,8 @@ class TestApp(unittest.TestCase):
 
     @patch('tkinter.messagebox.showerror')
     def test_pause_music_with_no_song_selected(self, mock_messagebox):
-        # test if an error message is displayed when no song is selected to
-        # pause
+        """test if an error message is displayed when no song is selected to
+        pause"""
         self.app.playlist.selection_clear(0, 'end')
         self.app.click_pause()
         mock_messagebox.assert_called_once_with('show error',
@@ -93,7 +96,7 @@ class TestApp(unittest.TestCase):
 
     @patch('tkinter.messagebox.showerror')
     def test_pause_music_with_no_song_playing(self, mock_messagebox):
-        # Ensure that an error message is displayed when no song is playing
+        """Ensure that an error message is displayed when no song is playing"""
         self.app.playlist.insert('end', 'risk.mp3')
         self.app.playlist.selection_set('end')
         self.app.click_play()
@@ -104,8 +107,8 @@ class TestApp(unittest.TestCase):
 
     @patch('tkinter.messagebox.showerror')
     def test_pause_music_with_selected_song_not_playing(self, mock_messagebox):
-        # Ensure that an error message is displayed when selected song is not
-        # playing
+        """Ensure that an error message is displayed when selected song is not
+        playing"""
         self.app.playlist.insert('end', 'risk.mp3')
         self.app.playlist.selection_set('end')
         self.app.click_play()
@@ -120,7 +123,7 @@ class TestApp(unittest.TestCase):
     @patch('pygame.mixer.music.load')
     @patch('pygame.mixer.music.play')
     def test_click_next(self, mock_play, mock_load):
-        # test if next song is played
+        """test if next song is played"""
         self.app.playlist.insert('end', 'risk.mp3')
         self.app.playlist.insert('end', 'drop-it.mp3')
         self.app.playlist.selection_set(0)
@@ -133,7 +136,7 @@ class TestApp(unittest.TestCase):
     @patch('pygame.mixer.music.load')
     @patch('pygame.mixer.music.play')
     def test_click_prev(self, mock_play, mock_load):
-        # test if previous song is played
+        """test if previous song is played"""
         self.app.playlist.insert('end', 'risk.mp3')
         self.app.playlist.insert('end', 'drop-it.mp3')
         self.app.playlist.selection_set(0)
@@ -144,7 +147,7 @@ class TestApp(unittest.TestCase):
         mock_play.assert_called_once_with()
 
     def test_click_delete(self):
-        # test if the song is deleted
+        """test if the song is deleted"""
         # add a song
         self.app.playlist.insert('end', 'risk.mp3')
         self.app.playlist.selection_set('end')
@@ -158,7 +161,8 @@ class TestApp(unittest.TestCase):
 
     @patch('tkinter.messagebox.showerror')
     def test_click_delete_with_no_song_selected(self, mock_messagebox):
-        # Ensure that an error message is displayed when no song is
+        """Ensure that an error message is displayed when no song is
+        selected"""
         self.app.click_delete()
         mock_messagebox.assert_called_once_with('show error',
                                                 'no song selected!')
